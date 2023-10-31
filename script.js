8a54b6ab5976283401a89ff9b23d4e6ac58fab90
Sujal Karmakar <sujalkarmakar6363@gmail.com>
2023-10-31 21:10:41 +0530

    done with animations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 Shery.imageEffect("#back", {
-  style: 5,
-  config: {
+    style: 5,
+    config: {
     a: { value: 2, range: [0, 30] },
     b: { value: -0.74, range: [-1, 1] },
     zindex: { value: -9996999, range: [-9999999, 9999999] },
@@ -17,7 +17,7 @@ Shery.imageEffect("#back", {
     geoVertex: { range: [1, 64], value: 1 },
     noEffectGooey: { value: false },
     onMouse: { value: 1 },
-    noise_speed: { value: 0.66, range: [0, 10] },
+    noise_speed: { value: 0.9, range: [0, 10] },
     metaball: { value: 0.2, range: [0, 2], _gsap: { id: 3 } },
     discard_threshold: { value: 0.5, range: [0, 1] },
     antialias_threshold: { value: 0, range: [0, 0.1] },
@@ -26,3 +26,36 @@ Shery.imageEffect("#back", {
   },
   gooey: true,
 });
+
+var elems = document.querySelectorAll(".elem")
+
+elems.forEach(function(elem){ //for every elem do the same to get the sliding animation for all the elems
+    var h1s = elem.querySelectorAll("h1")
+var index = 0;
+var animating = false; //if clicked vigorously the animation sucks so to fix that, if animation is ongoing dont force the animation again
+
+document.querySelector("#main").addEventListener("click", function(){
+    if(!animating){
+        animating = true;
+        gsap.to(h1s[index],{
+            top: '-=100%',
+            ease: Expo.easeInOut,
+            duration: 1,
+            onComplete: function(){
+                gsap.set(this._targets[0], {top:"100%"}) //to push the zeroth index h1 back to its orignal position that is -100%, if this line is ignored the h1 animations will not loop and will be blank after 5 clicks
+            animating = false;
+            },
+        });
+        index ===h1s.length-1? index = 0 : index++;
+        gsap.to(h1s[index],{
+            top: '-=100%',
+            ease: Expo.easeInOut,
+            duration: 1,
+        });
+    }
+});
+})
+
+
+
+
